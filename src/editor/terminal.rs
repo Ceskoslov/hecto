7c9b9dae0b6834dcdedd797ce5008746825454b4
xiwu Liu <56268425+cesko-web@users.noreply.github.com>
2025-09-16 22:27:48 +0900

    Improve Tildes

--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,34 +1,76 @@
-use crossterm::cursor::MoveTo;
+use crossterm::cursor::{MoveTo, Hide, Show};
+use crossterm::queue;
+use crossterm::style::Print;
 use crossterm::execute;
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType};
-use std::io::stdout;
+use std::io::{stdout, Error, Write};
 
-pub struct Terminal{}
+#[derive(Clone, Copy)]
+pub struct Size {
+    pub width: u16,
+    pub height: u16,
+}
+
+#[derive(Clone, Copy)]
+pub struct Position {
+    pub x: u16,
+    pub y: u16,
+}
+
+pub struct Terminal;
 
 impl Terminal{
-    pub fn terminate() -> Result<(), std::io::Error> {
+    pub fn terminate() -> Result<(), Error> {
+        Self::execute()?;
         disable_raw_mode()?;
         Ok(())
     }
 
-    pub fn initialize() -> Result<(), std::io::Error> {
+    pub fn initialize() -> Result<(), Error> {
         enable_raw_mode()?;
         Self::clear_screen()?;
-        Self::move_cursor_to(0, 0)?;
+        Self::move_cursor_to(Position{x:0,y: 0})?;
+        Self::execute()?;
         Ok(())
     }
 
-    pub fn clear_screen() -> Result<(), std::io::Error> {
+    pub fn clear_screen() -> Result<(), Error> {
         execute!(stdout(), Clear(ClearType::All))?;
         Ok(())
     }
 
-    pub fn move_cursor_to(x: u16, y: u16) -> Result<(), std::io::Error> {
-        execute!(stdout(), MoveTo(x, y))?;
+    pub fn clear_line() -> Result<(), Error> {
+        queue!(stdout(), Clear(ClearType::CurrentLine))?;
         Ok(())
     }
 
-    pub fn size() -> Result<(u16, u16), std::io::Error> {
-        size()
+    pub fn move_cursor_to(position: Position) -> Result<(), Error> {
+        queue!(stdout(), MoveTo(position.x, position.y))?;
+        Ok(())
+    }
+
+    pub fn hide_cursor() -> Result<(), Error> {
+        queue!(stdout(), Hide)?;
+        Ok(())
+    }
+
+    pub fn show_cursor() -> Result<(), Error> {
+        queue!(stdout(), Show)?;
+        Ok(())
+    }
+
+    pub fn print(s: &str) -> Result<(), Error> {
+        queue!(stdout(), Print(s))?;
+        Ok(())
+    }
+
+    pub fn size() -> Result<Size, std::io::Error> {
+        let (width,height) = size()?;
+        Ok((Size{height, width}))
+    }
+
+    pub fn execute() -> Result<(), Error> {
+        stdout().flush()?;
+        Ok(())
     }
 }
