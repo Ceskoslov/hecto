86628f57b20c36c00d9038cbf4c5e92817c6dc2d
xiwu Liu <56268425+cesko-web@users.noreply.github.com>
2025-09-16 22:55:37 +0900

    Welcome

--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,8 +1,8 @@
 use crossterm::cursor::{MoveTo, Hide, Show};
-use crossterm::queue;
+use crossterm::{queue, Command};
 use crossterm::style::Print;
-use crossterm::execute;
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType};
+use std::fmt::Display;
 use std::io::{stdout, Error, Write};
 
 #[derive(Clone, Copy)]
@@ -35,38 +35,43 @@ impl Terminal{
     }
 
     pub fn clear_screen() -> Result<(), Error> {
-        execute!(stdout(), Clear(ClearType::All))?;
+        Self::queue_command(Clear(ClearType::All))?;
         Ok(())
     }
 
     pub fn clear_line() -> Result<(), Error> {
-        queue!(stdout(), Clear(ClearType::CurrentLine))?;
+        Self::queue_command(Clear(ClearType::CurrentLine))?;
         Ok(())
     }
 
     pub fn move_cursor_to(position: Position) -> Result<(), Error> {
-        queue!(stdout(), MoveTo(position.x, position.y))?;
+        Self::queue_command(MoveTo(position.x, position.y))?;
         Ok(())
     }
 
     pub fn hide_cursor() -> Result<(), Error> {
-        queue!(stdout(), Hide)?;
+        Self::queue_command(Hide)?;
         Ok(())
     }
 
     pub fn show_cursor() -> Result<(), Error> {
-        queue!(stdout(), Show)?;
+        Self::queue_command(Show)?;
         Ok(())
     }
 
-    pub fn print(s: &str) -> Result<(), Error> {
-        queue!(stdout(), Print(s))?;
+    pub fn print<T: Display>(string: T) -> Result<(), Error> {
+        Self::queue_command(Print(string))?;
         Ok(())
     }
 
     pub fn size() -> Result<Size, std::io::Error> {
         let (width,height) = size()?;
         Ok((Size{height, width}))
+    }
+
+    fn queue_command<T: Command>(command: T) -> Result<(), Error> {
+        queue!(stdout(), command)?;
+        Ok(())
     }
 
     pub fn execute() -> Result<(), Error> {
