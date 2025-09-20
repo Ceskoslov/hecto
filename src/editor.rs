50033dc6f216464d297fd8ac42efeac3dca8682d
xiwu Liu <56268425+cesko-web@users.noreply.github.com>
2025-09-20 15:48:15 +0900

    Poor Line Viewer

--- src/editor.rs
+++ src/editor.rs
@@ -1,10 +1,9 @@
 use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
 mod terminal;
+mod view;
 use std::io::Error;
 use terminal::{Position, Size, Terminal};
-
-const NAME: &str = env!("CARGO_PKG_NAME");
-const VERSION: &str = env!("CARGO_PKG_VERSION");
+use view::View;
 
 pub struct Editor {
     should_quit: bool,
@@ -46,51 +45,18 @@ impl Editor {
             }
         }
     }
+    
     fn refresh_screen(&self) -> Result<(), Error> {
         Terminal::hide_cursor()?;
         if self.should_quit {
             Terminal::clear_screen()?;
             Terminal::print("Goodbye.\r\n")?;
         } else {
-            Self::draw_rows()?;
+            View::render()?;
             Terminal::move_cursor_to(Position{ x: 0, y: 0 })?;
         }
         Terminal::show_cursor()?;
         Terminal::execute()?;
         Ok(())
     }
-
-    fn draw_welcome_message() -> Result<(), Error> {
-        let mut welcome_message = format!("{} -- version {}", NAME, VERSION);
-        let width = Terminal::size()?.width;
-        let len = welcome_message.len();
-        #[allow(clippy::integer_division)]
-        let padding = width.saturating_sub(len) / 2;
-        let spaces = " ".repeat(padding.saturating_sub(1));
-        welcome_message = format!("{}{}", spaces, welcome_message);
-        welcome_message.truncate(width);
-        Terminal::print(&welcome_message)?;
-        Ok(())
-    }
-
-    fn draw_empty_row()-> Result<(), Error> {
-        Terminal::print("~")?;
-        Ok(())
-    }
-
-    fn draw_rows()-> Result<(), Error> {
-        let Size{ height, .. } = Terminal::size()?;
-        for current_row in 0..height {
-            Terminal::clear_line()?;
-            if current_row == height / 3 {
-                Self::draw_welcome_message()?;
-            } else {
-                Self::draw_empty_row()?;
-            }
-            if current_row.saturating_add(1) < height {
-                Terminal::print("\r\n")?;
-            }
-        }
-        Ok(())
-    }    
 }
