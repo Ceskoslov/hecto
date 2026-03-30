01364b2af67821fccf118e54bbff2a87b287ba49
Boxin Liu <boxin.liu@avanade.com>
2026-03-30 17:11:04 +0900

    simple deletion

--- src/editor/view.rs
+++ src/editor/view.rs
@@ -31,6 +31,8 @@ impl View {
             EditorCommand::Move(direction) => self.move_text_location(&direction),
             EditorCommand::Quit => {},
             EditorCommand::Insert(character) => self.insert_char(character),
+            EditorCommand::Backspace => self.backspace(),
+            EditorCommand::Delete => self.delete(),
         }
     }
     pub fn load(&mut self, file_name: &str) {
@@ -54,6 +56,16 @@ impl View {
         if grapheme_delta > 0 {
             self.move_right();
         }
+        self.needs_redraw = true;
+    }
+
+    fn backspace(&mut self) {
+        self.move_left();
+        self.delete();
+    }
+
+    fn delete(&mut self) {
+        self.buffer.delete(self.text_location);
         self.needs_redraw = true;
     }
 
@@ -216,7 +228,7 @@ impl View {
     fn move_left(&mut self) {
         if self.text_location.grapheme_index > 0 {
             self.text_location.grapheme_index -= 1;
-        } else {
+        } else if self.text_location.line_index > 0 {
             self.move_up(1);
             self.move_to_end_of_line();
         }
