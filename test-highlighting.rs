cd7e4b108fd2e3350dc19f1746200a448fc04bbb
Boxin Liu <boxin.liu@avanade.com>
2026-04-02 14:12:35 +0900

    colors for chars

--- test-highlighting.rs
+++ test-highlighting.rs
@@ -21,3 +21,8 @@
 
 // Invalid Literals:
 // 0b102 0x1G, 1o108, 0xxx
+
+// Valid/Should be highlighted:
+// '1' 'a' 'b' '👍' '\x1b', 'notacharacter' '\'', '\\' '1''2''3' '1'notchar'2'
+// Invalid/ should not be highlighted:
+// "a", 'b c'
