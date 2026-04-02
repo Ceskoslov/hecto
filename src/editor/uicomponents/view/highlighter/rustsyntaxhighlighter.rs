79540c30ea3b308a8cbdf7c39376e15ac00f7a12
Boxin Liu <boxin.liu@avanade.com>
2026-04-02 13:48:14 +0900

    numbers highlighting

--- src/editor/uicomponents/view/highlighter/rustsyntaxhighlighter.rs
+++ src/editor/uicomponents/view/highlighter/rustsyntaxhighlighter.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use unicode_segmentation::UnicodeSegmentation;
 
 use super::{Annotation, AnnotationType, Line, SyntaxHighlighter};
 use crate::prelude::*;
@@ -8,26 +9,66 @@ pub struct RustSyntaxHighlighter {
     highlights: HashMap<LineIdx, Vec<Annotation>>,
 }
 
-impl RustSyntaxHighlighter {
-    fn highlight_digits(line: &Line, result: &mut Vec<Annotation>) {
-  
-        line.chars().enumerate().for_each(|(idx, ch)| {
-  
-            if ch.is_ascii_digit() { 
-                result.push(Annotation {
-                    annotation_type: AnnotationType::Digit,
-                    start: idx,
-                    end: idx.saturating_add(1),
-                });
-            }
-        });
+fn is_valid_number(word: &str) -> bool {
+    if word.is_empty() {
+        return false;
     }
+    let mut chars = word.chars();
+
+    if let Some(first) = chars.next() {
+        if !first.is_ascii_digit() {
+            return false;
+        }
+    }
+
+    let mut seen_dot = false;
+    let mut seen_e = false;
+    let mut prev_was_digit = true;
+
+    for char in chars {
+        match char {
+            '0'..='9' => {
+                prev_was_digit = true;
+            }
+            '_' => {
+                if !prev_was_digit {
+                    return false;
+                }
+                prev_was_digit = false;
+            }
+            '.' => {
+                if seen_dot || seen_e || !prev_was_digit {
+                    return false;
+                }
+                seen_dot = true;
+                prev_was_digit = false;
+            }
+            'e' | 'E' => {
+                if seen_e || !prev_was_digit {
+                    return false;
+                }
+                seen_e = true;
+                prev_was_digit = false;
+            }
+            _ => return false,
+        }
+    }
+    prev_was_digit
+
 }
 
 impl SyntaxHighlighter for RustSyntaxHighlighter {
     fn highlight(&mut self, idx: LineIdx, line: &Line) {
         let mut result = Vec::new();
-        Self::highlight_digits(line, &mut result);
+        for (start_idx, word) in line.split_word_bound_indices(){
+            if is_valid_number(word) {
+                result.push(Annotation {
+                    start: start_idx,
+                    end: start_idx.saturating_add(word.len()),
+                    annotation_type: AnnotationType::Number,
+                });
+            }
+        }
         self.highlights.insert(idx, result);
     }
     fn get_annotations(&self, idx: LineIdx) -> Option<&Vec<Annotation>> {
